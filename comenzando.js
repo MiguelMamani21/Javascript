848908fa27f7f342f94a3cc0108ec592b185ccc8
Miguel Mamani <parapruebas293@gmail.com>
2023-07-07 18:28:16 -0400

    variables y operadores

--- comenzando.js
+++ comenzando.js
@@ -1,2 +1,94 @@
 //console.log("Hola Mundo");
-    console.log("!Hola, Mundo¡");
+// console.log("!Hola, Mundo¡");
+// la variables se asigna por var , const o let 
+    var a;
+    var c = 2;
+
+    //console.log(a); //error = undefined
+    a = 10;
+    //  console.log(a); //tiene valor
+
+// = es el operador de asignacion
+
+// asignar el valor de una variable a otra
+var a = 5;
+var b;
+b=a;
+//console.log(a);
+//console.log(b);
+
+//inicializar  variables
+
+var x = 9; //Inicializando la variable
+var miIdioma = "Español"; //Inicializando la variable
+
+var m; // la variable no esta inivializada
+
+var suma = 15 + 2;
+console.log("suma de = "+suma);
+
+var resta = 5 - 15;
+console.log("resta  = "+resta);
+
+var producto = -5 * -3;
+console.log("producto  = "+producto);
+
+var cociente = 3 / 0;
+console.log("cociente  = "+cociente);
+
+var miNumeroDecimal =23.4;
+var peso = 65.05;
+var multiplicacion = miNumeroDecimal*peso;
+//console.log(multiplicacion);
+
+producto = 8.9 * 6;
+console.log(producto);
+
+var resto = 22 % 5;
+console.log(resto);
+
+console.log("/////////////////////////////");
+console.log("/////////////////////////////");
+
+var librosComprados = 105;
+console.log(librosComprados); //inicialmente
+
+//opcion 1
+librosComprados = librosComprados+1;
+console.log(librosComprados); 
+//opcion 2
+librosComprados++;
+console.log(librosComprados); 
+
+console.log("/////////////////////////////");
+console.log("/////////////////////////////");
+
+var numeroEstudiantes = 105;
+console.log(numeroEstudiantes); //inicialmente
+
+//opcion 1
+numeroEstudiantes = numeroEstudiantes-1;
+console.log(numeroEstudiantes); 
+//opcion 2
+numeroEstudiantes--;
+console.log(numeroEstudiantes); 
+
+console.log("/////////////////////////////");
+console.log("/////////////////////////////");
+
+var s = 20;
+s  += 10;
+console.log(s);
+
+
+var d = 20;
+d  -= 5;
+console.log(d);
+
+var totalCompras = 10;
+totalCompras *=7;
+console.log(totalCompras);
+
+var totalSueldos = 1000;
+totalSueldos /=5;
+console.log(totalSueldos);
