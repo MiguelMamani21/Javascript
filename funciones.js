2f48e2e3c57d39c6d9755d2076e051e3a9475eda
Miguel Mamani <parapruebas293@gmail.com>
2023-07-25 17:59:49 -0400

    funciones y vectores

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function mostrarMensaje(x){
+    x="Mundo"
+    console.log("Hola "+ x);
+}
+function sumar(a,b){
+    var suma= a+b;
+    console.log(a + " + " + b + " = "+suma);
+}
+sumar(5,6);
---
+++ listacompras.js
@@ -0,0 +1,5 @@
+var miListaDeCompras = [["cereal", 3], ["leche", 2], ["galletas", 4], ["pan", 5], ["referesco", 7], ["pollo", 7]];
+//cereal
+console.log('Voy a comprar '+miListaDeCompras[0][1] +' unidades de '+miListaDeCompras[0][0]+ '.');
+//leche
+console.log('Voy a comprar '+miListaDeCompras[1][1] +' unidades de '+miListaDeCompras[1][0]+ '.');
