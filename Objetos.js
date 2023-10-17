8cd494fc1f54f31867f03baaae42dc732c205837
Miguel Mamaní <raulmiguelmtorrez0@gmail.com>
2023-10-17 19:55:21 -0400

    continuando

---
+++ Objetos.js
@@ -0,0 +1,7 @@
+var miPerro = {
+    "nombre":"Brita",
+    "edad": 5,
+    "peso": 6,
+    "raza": "Cooker"
+};
+console.log(miPerro.nombre);
--- funciones.js
+++ funciones.js
@@ -10,5 +10,11 @@ sumar(5,6);
 sumar(2,4);
 
 function contatenar(c1,c2,c3){
-    console.log()
+    console.log(c1+" "+c2+" "+c3)
 }
+contatenar("estoy", "aprendiendo ","a programar");
+
+function suma(a,b){
+    return a + b;
+}
+console.log(suma(5,2))
